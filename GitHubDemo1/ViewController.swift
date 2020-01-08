baac88b3d7996ea98cc4805ae615eb198c19c137
Igor Korshunov <igorkors@post.bgu.ac.il>
2020-01-08 10:42:29 +0200

    Initial Commit

---
+++ GitHubDemo1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubDemo1
+//
+//  Created by Igor Korshunov on 08/01/2020.
+//  Copyright © 2020 Igor Korshunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
